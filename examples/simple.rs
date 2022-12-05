b795a2d482307bb764a818d35ab6e8621bc6f60d
DSeeLP <46624152+DSeeLP@users.noreply.github.com>
2022-12-05 18:02:40 +0100

    Use proper logging framework

--- examples/simple.rs
+++ examples/simple.rs
@@ -2,17 +2,27 @@ use std::error::Error;
 
 use clap::Parser;
 
-use repellet::{CliProcessor, CommandHandler, DefaultErrorHandler, ExecutionContext};
+use repellet::{CliProcessor, CommandHandler, DefaultErrorHandler, ExecutionContext, TermReader};
+use simplelog::{Config, TermLogger};
 
 #[derive(Debug, Parser)]
 pub enum SimpleCli {
     Test { name: String },
     Clear,
+    Panic,
 }
 
 pub fn main() {
+    let reader = TermReader::new();
+    TermLogger::init(
+        log::LevelFilter::Info,
+        Config::default(),
+        simplelog::TerminalMode::Mixed,
+        simplelog::ColorChoice::Auto,
+    )
+    .unwrap();
     let processor: CliProcessor<SimpleCli> =
-        CliProcessor::new(MyCommandHandler {}, DefaultErrorHandler::default());
+        CliProcessor::new(reader, MyCommandHandler {}, DefaultErrorHandler::default());
     processor.run().unwrap();
 }
 
@@ -31,6 +41,7 @@ impl CommandHandler<SimpleCli> for MyCommandHandler {
             SimpleCli::Clear => {
                 ctx.editor.clear_scrollback().unwrap();
             }
+            SimpleCli::Panic => panic!("Panic Test"),
         }
         Ok(())
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::any::Any;
+use std::panic::catch_unwind;
+use std::sync::Mutex;
 use std::{error::Error, marker::PhantomData};
 
 use std::fmt::{Debug, Display};
@@ -7,34 +10,47 @@ use clap::Command;
 use clap::{error::ErrorKind, Error as ClapError};
 use reedline::{DefaultPrompt, ExternalPrinter, Prompt, Reedline, Signal};
 
+pub struct TermReader {
+    pub editor: Reedline,
+    pub prompt: Box<dyn Prompt + Send>,
+    pub external_printer: ExternalPrinter<String>,
+}
+
+impl TermReader {
+    pub fn new() -> TermReader {
+        let external_printer = ExternalPrinter::default();
+        let editor = Reedline::create().with_external_printer(external_printer.clone());
+        let prompt = DefaultPrompt::new();
+        Self {
+            editor,
+            prompt: Box::new(prompt),
+            external_printer,
+        }
+    }
+}
+
 pub struct CliProcessor<C: clap::Parser> {
     command_handler: Box<dyn CommandHandler<C> + Send>,
     error_handler: Box<dyn ErrorHandler + Send>,
     pub command: Command,
-    pub editor: Reedline,
-    pub prompt: Box<dyn Prompt + Send>,
-    pub printer: ExternalPrinter<String>,
+    pub reader: TermReader,
     _data: PhantomData<C>,
 }
 
 impl<C: clap::Parser + Debug> CliProcessor<C> {
     pub fn new(
+        reader: TermReader,
         command_handler: impl CommandHandler<C> + Send + 'static,
         error_handler: impl ErrorHandler + Send + 'static,
     ) -> Self {
         let mut command = C::command().multicall(true);
         command.build();
-        let printer = ExternalPrinter::default();
-        let editor = Reedline::create().with_external_printer(printer.clone());
-        let prompt = DefaultPrompt::new();
 
         Self {
             command_handler: Box::new(command_handler),
             error_handler: Box::new(error_handler),
             command,
-            editor,
-            prompt: Box::new(prompt),
-            printer,
+            reader,
             _data: PhantomData,
         }
     }
@@ -74,12 +90,16 @@ pub trait ErrorHandler {
     fn on_clap_error(&self, error: ClapError) {
         match ClapError::kind(&error) {
             ErrorKind::DisplayHelp | ErrorKind::DisplayVersion => {
-                println!("{}", error);
+                log::info!("{}", error);
             }
             _ => {
-                eprintln!("Invalid command {}", error);
+                log::info!("Invalid command {}", error);
             }
         }
+    }
+
+    fn on_panic(&self, err: Box<dyn Any + Send>) {
+        log::error!("Command execution panicked!");
     }
 }
 
@@ -92,16 +112,24 @@ impl<C: clap::Parser + Debug> CliProcessor<C> {
     pub fn run(mut self) -> Result<(), Box<dyn Error>> {
         let mut command = self.command.clone();
         loop {
-            let sig = self.editor.read_line(&*self.prompt);
+            let sig = self.reader.editor.read_line(&*self.reader.prompt);
             match sig {
                 Ok(Signal::Success(buffer)) => {
-                    self.execute_command(&mut command, &buffer);
+                    let mtx = Mutex::new(&mut self);
+                    let cmd_mtx = Mutex::new(&mut command);
+                    if let Err(err) = catch_unwind(|| {
+                        let mut command = cmd_mtx.lock().unwrap();
+                        mtx.lock().unwrap().execute_command(&mut *command, &buffer);
+                        // self.execute_command(&mut command, &buffer);
+                    }) {
+                        self.error_handler.on_panic(err);
+                    }
                 }
                 Ok(Signal::CtrlC | Signal::CtrlD) => {
                     self.error_handler.on_interrupt();
                 }
                 x => {
-                    println!("Signal: {:?}", x);
+                    log::error!("Reed failed: {:?}", x);
                 }
             }
         }
@@ -116,12 +144,12 @@ impl<C: clap::Parser + Debug> CliProcessor<C> {
             Ok(cli_raw) => {
                 if let Ok(cli) = C::from_arg_matches(&cli_raw) {
                     let mut context = ExecutionContext {
-                        editor: &mut self.editor,
-                        printer: &self.printer,
+                        editor: &mut self.reader.editor,
+                        printer: &self.reader.external_printer,
                         command,
                     };
                     if let Err(err) = self.command_handler.handle_command(&mut context, cli) {
-                        eprintln!("An error occurred while executing a command! {}", err);
+                        log::error!("An error occurred while executing a command! {}", err);
                     }
                 }
             }
